e70faf0cab4ff9b35d2c1eb8a70b8f777c3a9805
dy_55 <minidy20001001@outlook.com>
2020-08-14 14:04:57 +0800

    Add time offset to simulate USW Time

--- rename.py
+++ rename.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def rename(previous_name: str) -> str:
     name_list = previous_name.split('.')
 
-    now = datetime.utcnow()
+    now = datetime.utcnow() - timedelta(hours=7)
     name_list[-2] += f"-{now.strftime(r'%Y-%m-%d')}"
 
     return '.'.join(name_list)
